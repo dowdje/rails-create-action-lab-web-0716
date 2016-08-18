977044a0e82eb9d60c31ba9322884d41b971528f
dowdje <dowdje89@gmail.com>
2016-08-18 07:31:06 -0400

    Completed Create STudent Lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :new, :create]
   get '/student/:id', to: 'students#show', as: 'student'
+  # get '/students/new', to: 'students#new', as: 'new_student'
+  # post '/stdents', to: 'students#create'
 end
